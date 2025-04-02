2e740e14f8f7647d336c80e3d9cbc1ab2f3b50a4
nush <nush8653@gmail.com>
2025-04-02 10:49:38 -0700

    moved footer and header code into respective files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,39 +1,19 @@
-'use client'
+"use client";
 
 import LogInComponent from "@/components/LogInComponent";
+import FooterComponent from "@/components/ui/footerComponent";
+import HeaderComponent from "@/components/ui/headerComponent";
 
 export default function Home() {
   return (
     <div className="font-roboto m-0 max-h-screen">
-      <div className='bg-[#E1ECFF] rounded-2xl py-10 min-w-screen max-w-screen mb-6'>
-        <div className="mx-8 font-inter">
-        <p className=" text-[#1ABC9C]">Itinera-IO</p>
-          <div className="text-3xl">            
-            <p className="text-[#E67E22]">Votes In, </p>
-            <p className="text-[#E67E22]">Adventure Out.</p>
-            <p className="text-[#2C3E50]">Log In.</p>
-          </div>
-          
-        </div>
-        
-      </div>
-      
+      <HeaderComponent />
+
       <div className="bg-[#ECF0F1] rounded-2xl min-h-[28rem] min-w-[20rem] lg:min-h-[25rem] lg:max-w-[20rem] mx-4 px-4">
-
-        <LogInComponent />        
-
+        <LogInComponent />
       </div>
 
-
-
-      <div className=" bg-[#1ABC9C] min-w-screen max-w-screen py-5 rounded-t-4xl mt-8">
-        <div className="flex text-white text-3xl justify-center ">
-          <p>Hello, </p>
-          <p className="text-[#2C3E50]"> Itinerista</p>
-          <p>.</p>
-        </div>
-        
-      </div>
+      <FooterComponent />
     </div>
   );
 }
---
+++ src/components/ui/FooterComponent.tsx
@@ -0,0 +1,18 @@
+"use client";
+import React from "react";
+
+const FooterComponent = () => {
+  return (
+    <div>
+      <div className=" bg-[#1ABC9C] min-w-screen max-w-screen py-5 rounded-t-4xl mt-8">
+        <div className="flex text-white text-3xl justify-center ">
+          <p>Hello, </p>
+          <p className="text-[#2C3E50]"> Itinerista</p>
+          <p>.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default FooterComponent;
