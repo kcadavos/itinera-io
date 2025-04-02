d29ecb738cb149830f44f98d09dd038665de8685
nush <nush8653@gmail.com>
2025-04-02 14:03:14 -0700

    nush

---
+++ src/components/HeaderComponent.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <div className='bg-[#E1ECFF] rounded-2xl py-10 min-w-screen max-w-screen mb-6'>
+        <div className="mx-8 font-inter">
+        <p className=" text-[#1ABC9C]">Itinera-IO</p>
+          <div className="text-3xl">            
+            <p className="text-[#E67E22]">Votes In, </p>
+            <p className="text-[#E67E22]">Adventure Out.</p>
+            <p className="text-[#2C3E50]">Log In.</p>
+          </div>
+          
+        </div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default HeaderComponent
