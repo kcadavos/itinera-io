e86744bc30aef7fd12b3c737459cf2cde0541d05
Kass-S <kassiespacek101@gmail.com>
2025-04-08 11:43:11 -0700

    changed input to textarea for multiline text

--- src/app/(pages)/ItinerarySuggestionPages/AddSuggestionPage/page.tsx
+++ src/app/(pages)/ItinerarySuggestionPages/AddSuggestionPage/page.tsx
@@ -17,7 +17,7 @@ const AddSuggestionPage = () => {
 
             <div className="bg-[#ECF0F1] rounded-2xl min-h-[28rem] min-w-[20rem] lg:min-h-[25rem] lg:max-w-[20rem] mx-4 px-4 relative mb-40">
 
-                <div className='p-2 pt-4'>
+                <div className='p-2 pt-8'>
 
                     <div className='flex justify-start my-4'>
                         <div className=" mr-2"> 
@@ -48,11 +48,11 @@ const AddSuggestionPage = () => {
                             <img src="/assets/Icons/Orion_map-marker2.svg" alt="category" className="w-8" />
                         </div>
 
-                        <input type="text" placeholder='Details' className='bg-white rounded-md py-1 px-2 pb-40 w-full' maxLength={400}/>
+                        <textarea placeholder='Details' className='bg-white rounded-md py-1 px-2 pb-36 w-full resize-none' maxLength={350}></textarea>
                     </div>
                 </div>
 
-                <div className="flex justify-center mt-18 absolute -bottom-7 left-1/2 transform -translate-x-1/2">
+                <div className="flex justify-center absolute -bottom-7 left-1/2 transform -translate-x-1/2">
                     <button className="bg-[#E67E22] hover:bg-[#d56b0f] border-4 border-white text-xl text-white rounded-[2.5rem] p-3 cursor-pointer" >
                     <img
                         src="/assets/Icons/Orion_add-place_solid.svg"
