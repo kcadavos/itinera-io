03f4c60ade8fd1162390510318929dc4d0f12875
Kass-S <kassiespacek101@gmail.com>
2025-04-09 10:42:43 -0700

    commit

--- src/app/(pages)/HomePage/page.tsx
+++ src/app/(pages)/HomePage/page.tsx
@@ -5,8 +5,8 @@ import React from 'react'
 
 const HomePage = () => {
   //using router to display page to style
-  const router = useRouter();
-  router.push('/ItinerarySuggestionPages/AddSuggestionPage');
+  // const router = useRouter();
+  // router.push('/ItinerarySuggestionPages/AddSuggestionPage');
 
   return (
     <div className='bg-gray-500 h-screen'>
