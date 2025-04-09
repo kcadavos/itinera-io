3d15dbd241f3f08729ed510802e449d2a2045cae
Kass-S <kassiespacek101@gmail.com>
2025-04-09 10:39:01 -0700

    altered the header to fit text

--- src/components/HeaderComponent.tsx
+++ src/components/HeaderComponent.tsx
@@ -5,15 +5,11 @@ const HeaderComponent = () => {
     <div>
       <div className='bg-[#E1ECFF] min-h-[10rem] max-h-[10rem] lg:min-h-[13.2rem] lg:max-h-[13.2rem] pt-10 pb-5 min-w-screen max-w-screen mb-6'>
         <div className="mx-8 font-inter">
-        <p className=" text-[#1ABC9C]">Itinera-IO</p>
-          <div className="text-3xl">            
-            <p className="text-[#E67E22]">Votes In, </p>
-            <p className="text-[#E67E22]">Adventure Out.</p>
-            <p className="text-[#2C3E50]">Log In.</p>
-          </div>
-          
-        </div>
-        
+          <p className=" text-[#1ABC9C]">Itinera-IO</p>
+          <p>Hi name, lets plan for</p>
+          <p className='text-3xl text-[#E67E22]'>Place</p>
+          <p className='text-sm'>What are you excided about?</p>  
+        </div>  
       </div>
     </div>
   )
