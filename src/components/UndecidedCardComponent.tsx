548fe0c4cefdb65106cc01fd9573d2e1ae0b08d2
Kass-S <kassiespacek101@gmail.com>
2025-04-29 14:03:43 -0700

    Card updates

--- src/components/UndecidedCardComponent.tsx
+++ src/components/UndecidedCardComponent.tsx
@@ -27,16 +27,16 @@ const UndecidedCardComponent = ({ activities }:{activities:IUndecidedData[] | nu
   return (
     activities?.map((activity: IUndecidedData, idx: number) => (
       <div key={idx} className={`${bgColors[idx % bgColors.length]} p-4 my-2 mb-10 mx-8 rounded-bl-2xl rounded-tr-2xl relative`}>
-        <div className='flex justify-between'>
+        <div className='flex justify-between mb-3'>
 
           <div className='text-white'>
             <h3 className='text-2xl mb-1'>{activity.activity}</h3>
             <p className='text-md my-1'>{activity.address}</p>
-            <p className='text-sm my-2'>{activity.details}</p>
+            <p className='text-xs my-2'>{activity.details}</p>
           </div>
 
-          <div>
-            <img src={IconSwitch(activity.category)} alt="category" className=' w-10' />
+          <div className='mr-5'>
+            <img src={IconSwitch(activity.category)} alt="category" className=' w-15' />
           </div>
         </div>
         
