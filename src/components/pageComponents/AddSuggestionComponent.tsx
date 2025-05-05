66e2767909dbeab32d279c7ad820122443ed0589
Kass-S <kassiespacek101@gmail.com>
2025-05-05 16:24:49 -0700

    made it so add suggestion does not display when voting is closed

--- src/components/pageComponents/AddSuggestionComponent.tsx
+++ src/components/pageComponents/AddSuggestionComponent.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useSelectedTripIdContext } from "@/context/DataContext";
+import { useSelectedTripIdContext, useSelectedTripIsVotingOpenContext } from "@/context/DataContext";
 import { AddActivity } from "@/lib/services/ActivityServices";
 import React, { useState } from "react";
 import {
@@ -15,6 +15,7 @@ import { getToken } from "@/lib/services/DataServices";
 
 const AddSuggestionComponent = () => {
   const { selectedTripId } = useSelectedTripIdContext();
+  const {selectedTripIsVotingOpen} = useSelectedTripIsVotingOpenContext();
   const [activity, setActivity] = useState<string>("");
   const [category, setCategory] = useState<string>("");
   const [address, setAddress] = useState<string>("");
@@ -68,166 +69,176 @@ const AddSuggestionComponent = () => {
     <div>
       {/* mobile */}
       <div className="block lg:hidden">
-        <div className="bg-[#ECF0F1] rounded-2xl min-h-[28rem] min-w-[20rem] lg:min-h-[25rem] lg:max-w-[20rem] mx-4 sm:mx-16 px-4 relative mb-40">
-          <div className="p-2 pt-8">
-            <div className="flex justify-start my-4">
-              <div className=" mr-2">
-                <img
-                  src="/assets/Icons/Orion_pointer.svg"
-                  alt="activity"
-                  className="w-8"
+        {
+          selectedTripIsVotingOpen ?
+          <div className="bg-[#ECF0F1] rounded-2xl min-h-[28rem] min-w-[20rem] lg:min-h-[25rem] lg:max-w-[20rem] mx-4 sm:mx-16 px-4 relative mb-40">
+            <div className="p-2 pt-8">
+              <div className="flex justify-start my-4">
+                <div className=" mr-2">
+                  <img
+                    src="/assets/Icons/Orion_pointer.svg"
+                    alt="activity"
+                    className="w-8"
+                  />
+                </div>
+
+                <input
+                  type="text"
+                  placeholder="Activity"
+                  className="bg-white rounded-md py-1 px-2 w-full"
+                  onChange={(e) => setActivity(e.target.value)}
                 />
               </div>
 
-              <input
-                type="text"
-                placeholder="Activity"
-                className="bg-white rounded-md py-1 px-2 w-full"
-                onChange={(e) => setActivity(e.target.value)}
-              />
-            </div>
+              <div className="flex justify-start my-4">
+                <div className=" mr-2">
+                  <img
+                    src="/assets/Icons/Orion_travel-ticket.svg"
+                    alt="category"
+                    className="w-8"
+                  />
+                </div>
 
-            <div className="flex justify-start my-4">
-              <div className=" mr-2">
-                <img
-                  src="/assets/Icons/Orion_travel-ticket.svg"
-                  alt="category"
-                  className="w-8"
+                <DropdownMenu>
+                  <DropdownMenuTrigger asChild>
+                    <button className={`bg-white rounded-md py-1 px-2 w-full ${(categoryChoices == 'Adventure & Outdoors' || categoryChoices == 'Culture & History' || categoryChoices == 'Food & Drink' || categoryChoices == 'Relaxation & Wellness' || categoryChoices == 'Entertainment & Nightlife') ? 'text-black' : 'text-[#34495E]/60'}  text-start hover:border-1 hover:border-black `}>
+                      <div className="flex justify-between">
+                        {categoryChoices}
+
+                        <svg
+                          width="14"
+                          height="18"
+                          viewBox="0 0 16 14"
+                          fill="none"
+                          xmlns="http://www.w3.org/2000/svg"
+                          className="pt-1 pr-1"
+                        >
+                          <path
+                            d="M8.0002 14L0.205975 0.500001L15.7944 0.500002L8.0002 14Z"
+                            fill="#6A6A6A"
+                            fillOpacity="0.56"
+                          />
+                        </svg>
+                      </div>
+                    </button>
+                  </DropdownMenuTrigger>
+                  <DropdownMenuContent className="w-64 sm:w-100 md:w-136 lg:w-64 border-1 border-black">
+                    <DropdownMenuGroup className="text-[#34495E]/60">
+                      {category == "Adventure & Outdoors" ? (
+                        <DropdownMenuItem className="bg-[#ECF0F1]">
+                          Adventure & Outdoors
+                        </DropdownMenuItem>
+                      ) : (
+                        <DropdownMenuItem
+                          onClick={() => setCategory("Adventure & Outdoors")}
+                        >
+                          Adventure & Outdoors
+                        </DropdownMenuItem>
+                      )}
+                      {category == "Culture & History" ? (
+                        <DropdownMenuItem className="bg-[#ECF0F1]">
+                          Culture & History
+                        </DropdownMenuItem>
+                      ) : (
+                        <DropdownMenuItem
+                          onClick={() => setCategory("Culture & History")}
+                        >
+                          Culture & History
+                        </DropdownMenuItem>
+                      )}
+                      {category == "Food & Drink" ? (
+                        <DropdownMenuItem className="bg-[#ECF0F1]">
+                          Food & Drink
+                        </DropdownMenuItem>
+                      ) : (
+                        <DropdownMenuItem
+                          onClick={() => setCategory("Food & Drink")}
+                        >
+                          Food & Drink
+                        </DropdownMenuItem>
+                      )}
+                      {category == "Relaxation & Wellness" ? (
+                        <DropdownMenuItem className="bg-[#ECF0F1]">
+                          Relaxation & Wellness
+                        </DropdownMenuItem>
+                      ) : (
+                        <DropdownMenuItem
+                          onClick={() => setCategory("Relaxation & Wellness")}
+                        >
+                          Relaxation & Wellness
+                        </DropdownMenuItem>
+                      )}
+                      {category == "Entertainment & Nightlife" ? (
+                        <DropdownMenuItem className="bg-[#ECF0F1]">
+                          Entertainment & Nightlife
+                        </DropdownMenuItem>
+                      ) : (
+                        <DropdownMenuItem
+                          onClick={() => setCategory("Entertainment & Nightlife")}
+                        >
+                          Entertainment & Nightlife
+                        </DropdownMenuItem>
+                      )}
+                    </DropdownMenuGroup>
+                  </DropdownMenuContent>
+                </DropdownMenu>
+              </div>
+
+              <div className="flex justify-start my-4">
+                <div className=" mr-2">
+                  <img
+                    src="/assets/Icons/Orion_map-marker.svg"
+                    alt="category"
+                    className="w-8"
+                  />
+                </div>
+
+                <input
+                  type="text"
+                  placeholder="Address"
+                  className="bg-white rounded-md py-1 px-2 w-full"
+                  onChange={(e) => setAddress(e.target.value)}
                 />
               </div>
 
-              <DropdownMenu>
-                <DropdownMenuTrigger asChild>
-                  <button className={`bg-white rounded-md py-1 px-2 w-full ${(categoryChoices == 'Adventure & Outdoors' || categoryChoices == 'Culture & History' || categoryChoices == 'Food & Drink' || categoryChoices == 'Relaxation & Wellness' || categoryChoices == 'Entertainment & Nightlife') ? 'text-black' : 'text-[#34495E]/60'}  text-start hover:border-1 hover:border-black `}>
-                    <div className="flex justify-between">
-                      {categoryChoices}
+              <div className="flex justify-start my-4">
+                <div className=" mr-2">
+                  <img
+                    src="/assets/Icons/Orion_map-marker2.svg"
+                    alt="category"
+                    className="w-8"
+                  />
+                </div>
 
-                      <svg
-                        width="14"
-                        height="18"
-                        viewBox="0 0 16 14"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                        className="pt-1 pr-1"
-                      >
-                        <path
-                          d="M8.0002 14L0.205975 0.500001L15.7944 0.500002L8.0002 14Z"
-                          fill="#6A6A6A"
-                          fillOpacity="0.56"
-                        />
-                      </svg>
-                    </div>
-                  </button>
-                </DropdownMenuTrigger>
-                <DropdownMenuContent className="w-64 sm:w-100 md:w-136 lg:w-64 border-1 border-black">
-                  <DropdownMenuGroup className="text-[#34495E]/60">
-                    {category == "Adventure & Outdoors" ? (
-                      <DropdownMenuItem className="bg-[#ECF0F1]">
-                        Adventure & Outdoors
-                      </DropdownMenuItem>
-                    ) : (
-                      <DropdownMenuItem
-                        onClick={() => setCategory("Adventure & Outdoors")}
-                      >
-                        Adventure & Outdoors
-                      </DropdownMenuItem>
-                    )}
-                    {category == "Culture & History" ? (
-                      <DropdownMenuItem className="bg-[#ECF0F1]">
-                        Culture & History
-                      </DropdownMenuItem>
-                    ) : (
-                      <DropdownMenuItem
-                        onClick={() => setCategory("Culture & History")}
-                      >
-                        Culture & History
-                      </DropdownMenuItem>
-                    )}
-                    {category == "Food & Drink" ? (
-                      <DropdownMenuItem className="bg-[#ECF0F1]">
-                        Food & Drink
-                      </DropdownMenuItem>
-                    ) : (
-                      <DropdownMenuItem
-                        onClick={() => setCategory("Food & Drink")}
-                      >
-                        Food & Drink
-                      </DropdownMenuItem>
-                    )}
-                    {category == "Relaxation & Wellness" ? (
-                      <DropdownMenuItem className="bg-[#ECF0F1]">
-                        Relaxation & Wellness
-                      </DropdownMenuItem>
-                    ) : (
-                      <DropdownMenuItem
-                        onClick={() => setCategory("Relaxation & Wellness")}
-                      >
-                        Relaxation & Wellness
-                      </DropdownMenuItem>
-                    )}
-                    {category == "Entertainment & Nightlife" ? (
-                      <DropdownMenuItem className="bg-[#ECF0F1]">
-                        Entertainment & Nightlife
-                      </DropdownMenuItem>
-                    ) : (
-                      <DropdownMenuItem
-                        onClick={() => setCategory("Entertainment & Nightlife")}
-                      >
-                        Entertainment & Nightlife
-                      </DropdownMenuItem>
-                    )}
-                  </DropdownMenuGroup>
-                </DropdownMenuContent>
-              </DropdownMenu>
-            </div>
-
-            <div className="flex justify-start my-4">
-              <div className=" mr-2">
-                <img
-                  src="/assets/Icons/Orion_map-marker.svg"
-                  alt="category"
-                  className="w-8"
-                />
+                <textarea
+                  placeholder="Details"
+                  className="bg-white rounded-md py-1 px-2 pb-36 w-full resize-none"
+                  maxLength={350}
+                  onChange={(e) => setDetails(e.target.value)}
+                ></textarea>
               </div>
-
-              <input
-                type="text"
-                placeholder="Address"
-                className="bg-white rounded-md py-1 px-2 w-full"
-                onChange={(e) => setAddress(e.target.value)}
-              />
             </div>
 
-            <div className="flex justify-start my-4">
-              <div className=" mr-2">
+            <div className="flex justify-center absolute -bottom-7 left-1/2 transform -translate-x-1/2">
+              <button className="bg-[#E67E22] hover:bg-[#d56b0f] border-4 border-white text-xl text-white rounded-[2.5rem] p-3 cursor-pointer">
                 <img
-                  src="/assets/Icons/Orion_map-marker2.svg"
-                  alt="category"
-                  className="w-8"
+                  src="/assets/Icons/Orion_add-place_solid.svg"
+                  className="w-10"
+                  alt="add"
+                  onClick={addActivityFetch}
                 />
-              </div>
-
-              <textarea
-                placeholder="Details"
-                className="bg-white rounded-md py-1 px-2 pb-36 w-full resize-none"
-                maxLength={350}
-                onChange={(e) => setDetails(e.target.value)}
-              ></textarea>
+              </button>
             </div>
+          </div> :
+          <div className="text-center text-[#2C3E50]">
+            <p className="text-xl mb-4">Voting is closed.</p>
+            <p>Unable to add new activities.</p>
           </div>
+        }
 
-          <div className="flex justify-center absolute -bottom-7 left-1/2 transform -translate-x-1/2">
-            <button className="bg-[#E67E22] hover:bg-[#d56b0f] border-4 border-white text-xl text-white rounded-[2.5rem] p-3 cursor-pointer">
-              <img
-                src="/assets/Icons/Orion_add-place_solid.svg"
-                className="w-10"
-                alt="add"
-                onClick={addActivityFetch}
-              />
-            </button>
-          </div>
-        </div>
       </div>
+
+
     </div>
   );
 };
